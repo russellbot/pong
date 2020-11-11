1b664322000daf8400c5ff346a449c135ee6dcee
Russell Hart <russellbot@gmail.com>
2020-11-11 15:33:32 -0500

    added constants

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
 const gameOverEl = document.createElement('div');
 
+// Paddle
+const paddleHeight = 10;
+const paddleWidth = 50;
+const paddleDiff = 25;
+let paddleBottomX = 225;
+let paddleTopX = 225;
+let playerMoved = false;
+let paddleContact = false;
+
+// Ball
+let ballX = 250;
+let ballY = 350;
+const ballRadius = 5;
+
+// Speed
+let speedY;
+let speedX;
+let trajectoryX;
+let computerSpeed;
+
 // Render Everything on Canvas
 function renderCanvas() {
     // Canvas Background
