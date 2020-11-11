51936a2333b7995d9b5baa8940cf6963946ac333
Russell Hart <russellbot@gmail.com>
2020-11-11 16:30:15 -0500

    ball reset and ball move

--- script.js
+++ script.js
@@ -85,6 +85,68 @@ function ballReset() {
     paddleContact = false;
 }
 
+// Adjust Ball Movement
+function ballMove() {
+    // Vertical Speed
+    ballY += -speedY;
+    // Horizontal Speed
+    if (playerMoved && paddleContact) {
+      ballX += speedX;
+    }
+}
+
+// Determine What Ball Bounces Off, Score Points, Reset Ball
+function ballBoundaries() {
+    // Bounce off Left Wall
+    if (ballX < 0 && speedX < 0) {
+      speedX = -speedX;
+    }
+    // Bounce off Right Wall
+    if (ballX > width && speedX > 0) {
+      speedX = -speedX;
+    }
+    // Bounce off player paddle (bottom)
+    if (ballY > height - paddleDiff) {
+      if (ballX > paddleBottomX && ballX < paddleBottomX + paddleWidth) {
+        paddleContact = true;
+        // Add Speed on Hit
+        if (playerMoved) {
+          speedY -= 1;
+          // Max Speed
+          if (speedY < -5) {
+            speedY = -5;
+            computerSpeed = 6;
+          }
+        }
+        speedY = -speedY;
+        trajectoryX = ballX - (paddleBottomX + paddleDiff);
+        speedX = trajectoryX * 0.3;
+      } else if (ballY > height) {
+        // Reset Ball, add to Computer Score
+        ballReset();
+        computerScore++;
+      }
+    }
+    // Bounce off computer paddle (top)
+    if (ballY < paddleDiff) {
+      if (ballX > paddleTopX && ballX < paddleTopX + paddleWidth) {
+        // Add Speed on Hit
+        if (playerMoved) {
+          speedY += 1;
+          // Max Speed
+          if (speedY > 5) {
+            speedY = 5;
+          }
+        }
+        speedY = -speedY;
+      } else if (ballY < 0) {
+        // Reset Ball, add to Player Score
+        ballReset();
+        playerScore++;
+      }
+    }
+}
+
 // Called Every Frame
 function animate() {
     renderCanvas();
@@ -97,7 +159,7 @@ function animate() {
 function startGame() {
     playerScore = 0;
     computerScore = 0;
-    // ballReset();
+    ballReset();
     createCanvas();
     animate();
     canvas.addEventListener('mousemove', (e) => {
